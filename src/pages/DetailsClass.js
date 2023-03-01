faf0d1b0905f926aa39c0de0f5d830679f3c34cf
sunardi1511 <sunardi1511@gmail.com>
2023-03-01 21:30:17 +0800

    sidebarclass

--- src/pages/DetailsClass.js
+++ src/pages/DetailsClass.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Youtube from 'react-youtube';
 import { useDispatch, useSelector } from "react-redux";
 
@@ -11,15 +11,14 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const DetailsClass = () => {
 
-  const COURSES = useSelector(state => state.courses)
-  const [currentChapter, setCurrentChapter] = useState(COURSES?.data?.[params.class]?.chapters[0])
-  const [currentLessons, setCurrentLessons] = useState(currentChapter?.lessons?.[0])
   const params = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
+console.log('params', params)
+  const COURSES = useSelector(state => state.courses)
 
   useEffect(() => {
-    window.scroll(0, 0)
+    window.scroll(0,0)
 
     dispatch(statusCourses("loading"))
     courses.details(params.class).then(res => {
@@ -35,26 +34,19 @@ const DetailsClass = () => {
   if (COURSES.status === "loading") return <Loading />
   if (COURSES.status === "error") return <Centered>{COURSES?.message ?? "error here"}</Centered>
 
-  useEffect(() => {
-    if (
-      COURSES.status === "ok" 
-    ) {
-      // setCurrentChapter(
-      //   COURSES?.data?.[params.class]?.chapters?.find(
-      //     (chapters) => +chapters.id === +params.chapters
-      //   )
-      // )
-      // setCurrentLessons(
-      //   currentChapter?.lessons?.find(
-      //     (lessons) => lessons.video === params.uid)
+  let currentChapter, currentLessons
+  if (
+    COURSES.status === "ok" &&
+    COURSES?.data?.[params.class]?.chapters
+  ) {
+    currentChapter = COURSES?.data?.[params.class]?.chapters?.find(
+      (chapters) => +chapters.id === +params.chapters
+    ) ?? COURSES?.data?.[params.class]?.chapters[0];
 
-      // )
-    }
-  }, [COURSES, params, setCurrentChapter, setCurrentLessons])
-
-
-
-
+    currentLessons = currentChapter?.lessons?.find(
+      (lessons) => lessons.video === params.uid)
+      ?? currentChapter?.lessons?.[0]
+  }
   console.log('currentLessons', currentLessons)
 
   const nextVideo = () => { }
